c6c69424043bfa88ecf809ba7d7fa7b8e9d79666
Keith Palmer <kpalmer@dev1.uglyslug.com>
2014-01-23 11:40:59 -0500

    Example of sorting invoices.

--- docs/example_find_invoices.php
+++ docs/example_find_invoices.php
@@ -5,7 +5,7 @@ header('Content-Type: text/plain');
 require '../ChargeOverAPI.php';
 
 //This url should be specific to your ChargeOver instance
-$url = 'https://dev.consolibyte.com/api/v3';
+$url = 'http://dev.chargeover.com/api/v3';
 //$url = 'https://YOUR-INSTANCE-NAME.chargeover.com/api/v3';
 
 $authmode = ChargeOverAPI::AUTHMODE_HTTP_BASIC;
@@ -15,7 +15,7 @@ $password = '9vCJbmdZKSieVchyrRItFQw8MBN4lOH3';
 $API = new ChargeOverAPI($url, $authmode, $username, $password);
 
 //Find a customer by the ChargeOver customer ID
-$resp = $API->find(ChargeOverAPI_Object::TYPE_INVOICE, array( 'customer_id:EQUALS:10' ));
+$resp = $API->find(ChargeOverAPI_Object::TYPE_INVOICE, array( 'customer_id:EQUALS:254' ), array('invoice_id:DESC'));
 
 if (!$API->isError($resp))
 {
