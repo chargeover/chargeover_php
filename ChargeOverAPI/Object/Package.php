25904213c25b332e02762264f0bbf266b8711383
Keith Palmer <keith@rykelabs.com>
2014-06-06 14:56:41 -0400

    Example of yearly packages.

--- ChargeOverAPI/Object/Package.php
+++ ChargeOverAPI/Object/Package.php
@@ -5,4 +5,13 @@ class ChargeOverAPI_Object_Package extends ChargeOverAPI_Object
 	const PAYMETHOD_CREDITCARD = 'crd';
 	const PAYMETHOD_ACH = 'ach';
 	const PAYMENTHOD_INVOICE = 'inv';
+
+	const PAYCYCLE_WEEKLY = '1wk';
+	const PAYCYCLE_EVERYOTHERWEEK = '2wk';
+	const PAYCYCLE_TWOWEEKS = '2wk';
+	const PAYCYCLE_TWOMONTHS = '2mn';
+	const PAYCYCLE_MONTHLY = 'mon';
+	const PAYCYCLE_QUARTERLY = 'qtr';
+	const PAYCYCLE_YEARLY = 'yrl';
+	const PAYCYCLE_SIXMONTHS = 'six';
 }
--- docs/example_create_package.php
+++ docs/example_create_package.php
@@ -9,16 +9,20 @@ $url = 'http://dev.chargeover.com/api/v3';
 //$url = 'https://YOUR-INSTANCE-NAME.chargeover.com/api/v3';
 
 $authmode = ChargeOverAPI::AUTHMODE_HTTP_BASIC;
-$username = 'o0ptJLS2zwTmMl5dRKZG9kPxYDWVCgHs';
-$password = 'WNJxUswvOFh0RbpXrC6Tut82HE9yGgPQ';
+$username = 'JxI2LYptQkVDMbaFST8RNegzrqji9Wmh';
+$password = 'k5mSN0rb7KFHl4PBVDpLv2JfoG6qEQiW';
 
 $API = new ChargeOverAPI($url, $authmode, $username, $password);
 
 $Package = new ChargeOverAPI_Object_Package();
 $Package->setCustomerId(1);
 
+// By default, ChargeOver will create MONTHLY recurring packages - but you can change this:
+//$Package->setPaycycle('yrl');  // yearly
+//$Package->setPayCycle('qtr');  // quarterly
+// @todo more cycles docs 
+
 $LineItem = new ChargeOverAPI_Object_LineItem();
-$LineItem->setExternalKey('abcd1234');
 $LineItem->setItemId(2);
 $LineItem->setDescrip('Test of a description goes here.');
 
