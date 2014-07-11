6bc936460790e0d14fa50344732ef6b1c08587a3
Keith Palmer <keith@rykelabs.com>
2014-07-11 16:52:53 -0400

    Example of changing the payment cycle.

---
+++ docs/example_change_paycycle_for_contract.php
@@ -0,0 +1,52 @@
+<?php
+
+header('Content-Type: text/plain');
+
+require '../ChargeOverAPI.php';
+
+//This url should be specific to your ChargeOver instance
+$url = 'http://dev.chargeover.com/api/v3';
+//$url = 'https://YOUR-INSTANCE-NAME.chargeover.com/api/v3';
+
+$authmode = ChargeOverAPI::AUTHMODE_HTTP_BASIC;
+$username = 'JxI2LYptQkVDMbaFST8RNegzrqji9Wmh';
+$password = 'k5mSN0rb7KFHl4PBVDpLv2JfoG6qEQiW';
+
+$API = new ChargeOverAPI($url, $authmode, $username, $password);
+
+$data = array(
+	// Valid options:
+	//   "mon"    // monthly
+	//   "yrl"    // yearly
+	//   "1wk"    // every week
+	//   "2wk"    // every other week
+	//   "qtr"    // every quarter
+	
+	'paycycle' => 'mon', 
+	);
+
+$resp = $API->action('package', 557, 'paycycle', $data);
+
+/*
+print("\n\n\n\n");
+print($API->lastRequest());
+print("\n\n\n\n");
+print($API->lastResponse());
+print("\n\n\n\n");
+*/
+
+if (!$API->isError($resp))
+{
+	print('SUCCESS!');
+}
+else
+{
+	print('Error message was: ' . $resp->code . ': ' . $resp->message . "\n");
+
+	print("\n\n\n\n");
+	print($API->lastRequest());
+	print("\n\n\n\n");
+	print($API->lastResponse());
+	print("\n\n\n\n");
+}
+
