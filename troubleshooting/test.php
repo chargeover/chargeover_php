27929472a3c98e7bfbdabcad1e11794397dc341f
Keith Palmer <keith@academickeys.com>
2015-05-19 22:17:58 -0400

    Troubleshooting.

--- troubleshooting/test.php
+++ troubleshooting/test.php
@@ -2,12 +2,15 @@
 
 header('Content-Type: text/plain');
 
-$ch = curl_init('https://how-late.chargeover.com/api/v3/customer');
+$ch = curl_init('https://demo.chargeover.com/api/v3/customer');
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_CIPHER_LIST, 'TLSv1');
 
 curl_setopt($ch, CURLOPT_VERBOSE, true);
+
+$fp = fopen('php://output', 'w+');
+curl_setopt($ch, CURLOPT_STDERR, $fp);
 
 $out = curl_exec($ch);
 
