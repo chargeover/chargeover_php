7293f2dd13070e52f597d4ba0507fae17a72f8ff
Keith Palmer <keith@rykelabs.com>
2021-05-17 13:40:37 -0400

    Work on #14, removing example credentials.

--- docs/config.php
+++ docs/config.php
@@ -1,10 +1,10 @@
 <?php
 
 //This url should be specific to your ChargeOver instance
-$url = 'http://dev1.chargeover.test/api/v3';
+$url = 'https://example.chargeover.test/api/v3';
 //$url = 'https://YOUR-INSTANCE-NAME.chargeover.com/api/v3';
 
 // Your ChargeOver API credentials
 $authmode = ChargeOverAPI::AUTHMODE_HTTP_BASIC;
-$username = 'jSTEk3V7ZWPUwQlm1bfCpOqxvBAcozdM';
-$password = 'DpRmGNS5Fw1vQrz8fyOKhc2AHijYnLIt';
+$username = '';
+$password = '';
